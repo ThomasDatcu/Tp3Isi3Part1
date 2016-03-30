7a21670604a30d2dcbe6bfd164a17c3fbb1a14a7
ThomasDatcu <thomasdatcu@hotmail.fr>
2016-03-30 08:49:18 +0200

    src file

--- test/src/persons/TestPerson.java
+++ test/src/persons/TestPerson.java
@@ -1,6 +1,12 @@
 package persons;
 
-public class TestPerson {
+
+public class TestPerson extends JUnit {
+
+	/*
+	Question 1 : Analyse partitionnelle
+	pour
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
